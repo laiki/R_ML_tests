1afa55fac47dcc22e2b75df98c2d80a35ff2eda9
Wasilios Goutas <wasili@goutas.de>
2020-03-24 19:30:14 +0100

    autokeras adaptions, added RNN test

--- workspace.R
+++ workspace.R
@@ -29,100 +29,137 @@
 
 # h2o ----
 {
-  h2o_automl_test <- function(data, .max_runtime_secs = 60 * 10, test = test_tbl){
-    library(h2o)
-    library(lime)
-    h2o.init()
+  h2o_automl_test <- function(data=testData, .max_runtime_secs = 60 * 3, test = test_tbl){
+    #set-up ----
+    {
+      library(h2o)
+      library(lime)
+      h2o.init(port = 12345)
+    }
     
-    aml <- h2o.automl( x = grep( pattern = 'x.', x = names(data)), #indices of features
-                       y = grep( pattern = 'y' , x = names(data)), #indices of target (will be always 1)
-                       training_frame = as.h2o(data),
-                       nfolds = 5,
-                       max_runtime_secs = .max_runtime_secs )
-    model <- aml@leaderboard %>% 
-      as_tibble() %>% 
-      slice(1) %>% 
-      pull(model_id) %>% 
-      h2o.getModel()
-    h2o.performance(model = model, xval = TRUE)
-    dir.create( path = model_filepath <- paste('models', 'h2o', 'automl', sep = '/'), showWarnings = F, recursive = T)
-    h2o.saveModel(model, model_filepath, force = TRUE)
+    # modeling ----
+    {
+      aml <- h2o.automl( x = grep( pattern = 'x.', x = names(data)), #indices of features
+                         y = grep( pattern = 'y' , x = names(data)), #indices of target (will be always 1)
+                         training_frame = as.h2o(data),
+                         nfolds = 5,
+                         max_runtime_secs = .max_runtime_secs )
+      model <- aml@leaderboard %>% 
+        as_tibble() %>% 
+        slice(1) %>% 
+        pull(model_id) %>% 
+        h2o.getModel()
+    }
     
-    #explainer   <- lime (data, model)
-    #explanation <- explain(test, explanation, n_features = 5, feature_select = "highest_weights")
-    #p <- plot_explanations(explanation) # not working :(
-    #ggplot2::ggsave(filename = paste(model_filepath, lime.plot.png, sep = '/'), plot = p)
-    h2o.shutdown(prompt = F)  
+    # evaluation ----
+    {
+      h2o.performance(model = model, xval = TRUE)
+      #explainer   <- lime (data, model)
+      #explanation <- explain(test, explanation, n_features = 5, feature_select = "highest_weights")
+      #p <- plot_explanations(explanation) # not working :(
+      
+    }
+    
+    # store ----
+    {
+      #ggplot2::ggsave(filename = paste(model_filepath, lime.plot.png, sep = '/'), plot = p)
+      dir.create( path = model_filepath <- paste('models', 'h2o', 'automl', sep = '/'), showWarnings = F, recursive = T)
+      h2o.saveModel(model, model_filepath, force = TRUE)
+    }
+    
+    # clean-up ----
+    {
+      h2o.shutdown(prompt = F)  
+    }
+    return(model_filepath)
   }
 }
 # autokeras ----
 { 
   setUpAutokeras <- function(){
-    library(reticulate)
-    if( !('r-reticulate' %in% reticulate::conda_list(conda = '/opt/conda/bin/conda')$name) ){          
-      reticulate::conda_create(envname = 'r-reticulate', packages = 'python=3.6', conda = '/opt/conda/bin/conda')
+    if(! ("autokeras" %in% (installed.packages() %>% as_tibble())$Package) ){
+      install.packages('autokeras')
     }
-    reticulate::use_condaenv(condaenv = 'r-reticulate', conda = '/opt/conda/bin/conda')
+    library(reticulate)
+    if( !('autokeras' %in% reticulate::conda_list(conda = '/opt/conda/bin/conda')$name) ){          
+      reticulate::conda_create(envname = 'autokeras', packages = 'python=3.6', conda = '/opt/conda/bin/conda')
+    }
+    reticulate::use_condaenv(condaenv = 'autokeras', conda = '/opt/conda/bin/conda')
     
     library(autokeras)
     library(keras)
-    autokeras::install_autokeras( method = 'conda',                                        
+    autokeras::install_autokeras( method = 'virtualenv',                                        
                                   conda = '/opt/conda/bin/conda',                          
                                   tensorflow = '2.1.0-gpu',                                
                                   version = 'default' )
   }
   
-  autokeras_test <- function(data, .max_trials = 10, .epochs = 10, test = test_tbl){
-    library(autokeras)
-    library(keras)
-    library(reticulate)
-    library(ggplot2)
-    reticulate::use_condaenv(condaenv = 'r-reticulate', conda = '/opt/conda/bin/conda')
+  autokeras_test <- function(data=train_tbl, .max_trials = 10, .epochs = 10, test = test_tbl){
+    # set-up ----
+    {
+      setUpAutokeras()
+      library(autokeras)
+      library(keras)
+      library(reticulate)
+      library(ggplot2)
+      #reticulate::use_condaenv(condaenv = 'autokeras', conda = '/opt/conda/bin/conda')
+      reticulate::use_virtualenv()
+    }
+    model <- NULL
+    # modeling ----
+    {
+      reg <- model_structured_data_regressor(
+        column_names = grep(pattern = 'x.', x = names(data), value = T),
+        loss = "mean_squared_error",
+        max_trials = .max_trials,
+        objective = "val_loss",
+        overwrite = TRUE,
+        seed = runif(1, 0, 1e+06) ) 
+      
+      tensorboard("models/logs/run_autokeras")
+      model <- 
+        fit( object = reg, 
+             x = as_tibble(data[ , grep( pattern = 'x.', x = names(data))]), #tibble of features
+             y = as_tibble(data[ , grep( pattern = 'y' , x = names(data))]), # target values
+             epochs = .epochs,
+             callbacks = list (
+               keras::callback_tensorboard("models/logs/run_autokeras"),
+               #           keras::callback_reduce_lr_on_plateau(monitor = "val_loss", factor = 0.01),
+               keras::callback_early_stopping(min_delta = 0.0001, restore_best_weights = TRUE, verbose = T)
+             ),
+             validation_split = 0.2
+        )
+    }
     
-    reg <- model_structured_data_regressor(
-      column_names = grep(pattern = 'x.', x = names(data), value = T),
-      loss = "mean_squared_error",
-      max_trials = .max_trials,
-      objective = "val_loss",
-      overwrite = TRUE,
-      seed = runif(1, 0, 1e+06) ) 
-    
-    tensorboard("models/logs/run_autokeras")
-    model <- 
-      fit( object = reg, 
-           x = as_tibble(data[ , grep( pattern = 'x.', x = names(data))]), #tibble of features
-           y = as_tibble(data[ , grep( pattern = 'y' , x = names(data))]), # target values
-           epochs = .epochs,
-           callbacks = list (
-             keras::callback_tensorboard("models/logs/run_autokeras"),
-             #           keras::callback_reduce_lr_on_plateau(monitor = "val_loss", factor = 0.01),
-             keras::callback_early_stopping(min_delta = 0.0001, restore_best_weights = TRUE, verbose = T)
-           ),
-           validation_split = 0.2
+    # evaluation ----
+    {
+      # Predict with the best model
+      predicted <- tibble(idx      = seq(1:nrow(test)), 
+                          value    = predict(model, test[ , grep( pattern = 'x.', x = names(data))]), 
+                          variable = 'predicted' )
+      result <- rbind( tibble(idx      = seq(1:nrow(test)), 
+                              value    = test$y, 
+                              variable = 'value' ),
+                       predicted ) %>% 
+        arrange(idx) %>%
+        select(idx, variable, value)
+      
+      p <- result %>% ggplot(aes(idx, value, colour = variable)) + geom_line()
+      # Evaluate the best model with testing data
+      model %>% evaluate(
+        x = as_tibble(test_tbl[ , grep( pattern = 'x.', x = names(data))]), #tibble of features
+        y = as_tibble(test_tbl[ , grep( pattern = 'y' , x = names(data))])  # target values
       )
+    }
     
-    # Predict with the best model
-    predicted <- tibble(idx      = seq(1:nrow(test)), 
-                        value    = predict(model, test[ , grep( pattern = 'x.', x = names(data))]), 
-                        variable = 'predicted' )
-    result <- rbind( tibble(idx      = seq(1:nrow(test)), 
-                            value    = test$y, 
-                            variable = 'value' ),
-                     predicted ) %>% 
-      arrange(idx) %>%
-      select(idx, variable, value)
-    
-    p <- result %>% ggplot(aes(idx, value, colour = variable)) + geom_line()
-    # Evaluate the best model with testing data
-    model %>% evaluate(
-      x = as_tibble(test_tbl[ , grep( pattern = 'x.', x = names(data))]), #tibble of features
-      y = as_tibble(test_tbl[ , grep( pattern = 'y' , x = names(data))])  # target values
-    )
-    
-    # save the model
-    dir.create( path = dirname( model_filepath <- paste('models', 'autokeras', 'autokeras.model', sep = '/') ), showWarnings = F, recursive = T) 
-    autokeras::save_model(autokeras_model = model, filename = model_filepath)
-    #nvidia-smi pmon -c 1 --select m | grep rsession
+    # store ----
+    {
+      # save the model
+      dir.create( path = dirname( model_filepath <- paste('models', 'autokeras', 'autokeras.model', sep = '/') ), showWarnings = F, recursive = T) 
+      autokeras::save_model(autokeras_model = model, filename = model_filepath)
+      #nvidia-smi pmon -c 1 --select m | grep rsession
+    }
+    return (model)
   }
 }
 
@@ -171,47 +208,50 @@
   }
   
   basicTFtest <- function(data = testData){
-    library(reticulate)
-    use_condaenv(condaenv = 'r-reticulate', conda = '/opt/conda/bin/conda')
-    train_gen <- generator(
-      as.matrix(data),
-      lookback = lookback,
-      delay = delay,
-      min_index = 1,
-      max_index = floor(nrow(data)*(8/10)),
-      shuffle = FALSE,
-      step = step,
-      batch_size = batch_size
-    )
-    
-    
-    val_gen <- generator(
-      as.matrix(data),
-      lookback = lookback,
-      delay = delay,
-      min_index = floor(nrow(data)*(8/10)) + 1,
-      max_index = floor(nrow(data)*(9/10)),
-      step = step,
-      batch_size = batch_size
-    )
-    
-    
-    test_gen <- generator(
-      as.matrix(data),
-      lookback = lookback,
-      delay = delay,
-      min_index = floor(nrow(data)*(9/10)) + 1,
-      max_index = nrow(data),
-      step = step,
-      batch_size = batch_size
-    )
-    
-    
-    # # How many steps to draw from val_gen in order to see the entire validation set
-    val_steps <- (floor(nrow(data)*(9/10)) - floor(nrow(data)*(8/10)) + 1 - lookback) / batch_size
-    # 
-    # # How many steps to draw from test_gen in order to see the entire test set
-    test_steps <- (nrow(data) - floor(nrow(data)*(9/10)) + 1 - lookback) / batch_size
+    # set up ----
+    {
+      library(reticulate)
+      use_condaenv(condaenv = 'r-reticulate', conda = '/opt/conda/bin/conda')
+      train_gen <- generator(
+        as.matrix(data),
+        lookback = lookback,
+        delay = delay,
+        min_index = 1,
+        max_index = floor(nrow(data)*(8/10)),
+        shuffle = FALSE,
+        step = step,
+        batch_size = batch_size
+      )
+      
+      
+      val_gen <- generator(
+        as.matrix(data),
+        lookback = lookback,
+        delay = delay,
+        min_index = floor(nrow(data)*(8/10)) + 1,
+        max_index = floor(nrow(data)*(9/10)),
+        step = step,
+        batch_size = batch_size
+      )
+      
+      
+      test_gen <- generator(
+        as.matrix(data),
+        lookback = lookback,
+        delay = delay,
+        min_index = floor(nrow(data)*(9/10)) + 1,
+        max_index = nrow(data),
+        step = step,
+        batch_size = batch_size
+      )
+      
+      
+      # # How many steps to draw from val_gen in order to see the entire validation set
+      val_steps <- (floor(nrow(data)*(9/10)) - floor(nrow(data)*(8/10)) + 1 - lookback) / batch_size
+      # 
+      # # How many steps to draw from test_gen in order to see the entire test set
+      test_steps <- (nrow(data) - floor(nrow(data)*(9/10)) + 1 - lookback) / batch_size
+    }
     
     library(keras)
     model <- keras_model_sequential() %>% 
@@ -234,16 +274,94 @@
       validation_steps = val_steps,
       callbacks = callback_tensorboard("models/logs/run_basicTF")
     )
+    # this will result in an error when using TensorFlow 2.1.0 as described in bug 36919
+    # https://github.com/tensorflow/tensorflow/issues/36919
+    # but it works with TensorFlow 2.0.0
+    
+    return (model)
   }
+
+  basicRNNtest <- function(data = testData){
+    # set up ----
+    {
+      library(reticulate)
+      use_condaenv(condaenv = 'r-reticulate', conda = '/opt/conda/bin/conda')
+      train_gen <- generator(
+        as.matrix(data),
+        lookback = lookback,
+        delay = delay,
+        min_index = 1,
+        max_index = floor(nrow(data)*(8/10)),
+        shuffle = FALSE,
+        step = step,
+        batch_size = batch_size
+      )
+      
+      
+      val_gen <- generator(
+        as.matrix(data),
+        lookback = lookback,
+        delay = delay,
+        min_index = floor(nrow(data)*(8/10)) + 1,
+        max_index = floor(nrow(data)*(9/10)),
+        step = step,
+        batch_size = batch_size
+      )
+      
+      
+      test_gen <- generator(
+        as.matrix(data),
+        lookback = lookback,
+        delay = delay,
+        min_index = floor(nrow(data)*(9/10)) + 1,
+        max_index = nrow(data),
+        step = step,
+        batch_size = batch_size
+      )
+      
+      
+      # # How many steps to draw from val_gen in order to see the entire validation set
+      val_steps <- (floor(nrow(data)*(9/10)) - floor(nrow(data)*(8/10)) + 1 - lookback) / batch_size
+      # 
+      # # How many steps to draw from test_gen in order to see the entire test set
+      test_steps <- (nrow(data) - floor(nrow(data)*(9/10)) + 1 - lookback) / batch_size
+    }
+    
+    library(keras)
+    model <- keras_model_sequential() %>% 
+      layer_gru(units = 32, input_shape = list(NULL, dim(data)[[-1]])) %>% 
+      layer_dense(units = 1)
+    # at this point rsession needs 10GB more of GPU memory
+    
+    model %>% compile(
+      optimizer = optimizer_rmsprop(),
+      loss = "mae"
+    )
+    
+    tensorboard("models/logs/run_basicTF")
+    history <- model %>% fit_generator(
+      train_gen,
+      steps_per_epoch = 500,
+      epochs = 20,
+      validation_data = val_gen,
+      validation_steps = val_steps,
+      callbacks = callback_tensorboard("models/logs/run_basicTF")
+    )
+
+    return (model)
+  }
+  
 }
 
 
 
 
+
 # run tests ----
-#  train_tbl %>% h2o_automl_test()
+#  h2o_automl_test()
 
 # setUpAutokeras() # run this once to install the right tensorflow version 
 # train_tbl %>% autokeras_test()
 
 # basicTFtest()
+# basicRNNtest()
